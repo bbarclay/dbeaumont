89656c45fa6cb60b622baef1e672b78dbfd641a0
julycabigas <july@businessblueprint.com>
2018-01-17 14:16:18 +0800

    update js

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -116,6 +116,76 @@ $(document).ready(function(){
 					.setClassToggle(".step-2", "fadeInRight") // add class toggle
 					.addTo(controller4);	
 
+		}
+		else {
+
+			var is_keynoteSK = document.querySelector('.keynote-sk');
+
+			if(is_keynoteSK) {
+
+			 var controller = new ScrollMagic.Controller();
+
+			 var keynoteItem = document.querySelectorAll('.keynotes .item');
+			 var item_info1 = document.querySelector('.keynotes .item1 .info');
+			 var item_image1 = document.querySelector('.keynotes .item1 .image');
+			 var item_bgInfo1 = document.querySelector('.keynotes .item1 .bg');
+			 var tw_item = '';
+
+			  // tw_item = new TimelineLite();
+ 			 //  tw_item.staggerFromTo(keynoteItem, 0.8, {autoAlpha: 0}, {autoAlpha:1, onUpdate: animateTW}, 0.3)	
+
+
+
+				 var keynote_tw = new TimelineLite();
+
+			     keynote_tw
+			        .fromTo(item_image1, 0.5, { autoAlpha: 0, x: -100 }, {autoAlpha:1, x: 0})
+			        .fromTo(item_bgInfo1, 0.5, { autoAlpha: 0, x: 300 }, {autoAlpha:1, x: 0}, '-=0.2')
+			     	.fromTo(item_info1, 0.5, { autoAlpha: 0, x: 100 }, {autoAlpha:1, x: 0});
+
+		
+			        new ScrollMagic.Scene({triggerElement: '.keynotes .item1', triggerHook: 0.9})
+			        					.setTween(keynote_tw)
+			        					.addTo(controller);
+
+
+			 var item_info2 = document.querySelector('.keynotes .item2 .info');
+			 var item_image2 = document.querySelector('.keynotes .item2 .image');
+			 var item_bgInfo2 = document.querySelector('.keynotes .item2 .bg');
+
+
+				 var keynote_tw2 = new TimelineLite();
+
+			     keynote_tw2
+			        .fromTo(item_image2, 0.5, { autoAlpha: 0, x: -100 }, {autoAlpha:1, x: 0})
+			        .fromTo(item_bgInfo2, 0.5, { autoAlpha: 0, x: 300 }, {autoAlpha:1, x: 0}, '-=0.2')
+			     	.fromTo(item_info2, 0.5, { autoAlpha: 0, x: 100 }, {autoAlpha:1, x: 0});
+
+		
+			        new ScrollMagic.Scene({triggerElement: '.keynotes .item2', triggerHook: 0.9})
+			        					.setTween(keynote_tw2)
+			        					.addTo(controller);
+
+			 var item_info3 = document.querySelector('.keynotes .item3 .info');
+			 var item_image3 = document.querySelector('.keynotes .item3 .image');
+			 var item_bgInfo3 = document.querySelector('.keynotes .item3 .bg');
+
+
+				 var keynote_tw3 = new TimelineLite();
+
+			     keynote_tw3
+			        .fromTo(item_image3, 0.5, { autoAlpha: 0, x: -100 }, {autoAlpha:1, x: 0})
+			        .fromTo(item_bgInfo3, 0.5, { autoAlpha: 0, x: 300 }, {autoAlpha:1, x: 0}, '-=0.2')
+			     	.fromTo(item_info3, 0.5, { autoAlpha: 0, x: 100 }, {autoAlpha:1, x: 0});
+
+		
+			        new ScrollMagic.Scene({triggerElement: '.keynotes .item3', triggerHook: 0.9})
+			        					.setTween(keynote_tw3)
+			        					.addTo(controller);
+
+
+			}
+			        
 		};
 
 	});
