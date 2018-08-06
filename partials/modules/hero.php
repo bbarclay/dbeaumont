ebce9c2ad2e6adaf96a2324fcc7b976d367ad774
julycabigas <july@businessblueprint.com>
2018-08-06 12:51:46 +0800

    update style

--- partials/modules/hero.php
+++ partials/modules/hero.php
@@ -1,8 +1,8 @@
 <?php $style = get_sub_field( 'image' ) ? 'style="background-image: url(' . get_sub_field( 'image' ) . ');"' : ''; ?>
-<section id="hero" class="module module--hero" <?php echo $style; ?>>
+<section id="hero" class="module module--hero <?php echo ( get_sub_field( 'text_left_align' ) ) ? 'align-left': '' ?>" <?php echo $style; ?>>
 	<div class="container">
 		<div class="row">
-			  <div class="col-md-offset-6 col-md-6 col-lg-offset-4 col-lg-8">
+			  <div class="col-md-6 col-lg-8 <?php echo ( get_sub_field( 'text_left_align' ) ) ? '': 'col-md-offset-6 col-lg-offset-4' ?>">
 			  	  <?php the_sub_field( 'header_text' ); ?>
 			  </div>
 		</div>
