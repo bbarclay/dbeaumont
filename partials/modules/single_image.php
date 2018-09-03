3b28404cdc2991e22b0f0a1a2b9aa547d85f0adb
julycabigas <july@businessblueprint.com>
2018-09-03 09:00:48 +0800

    update

--- partials/modules/single_image.php
+++ partials/modules/single_image.php
@@ -7,7 +7,26 @@
    	    	<?php $imageID = get_sub_field('image'); ?>
 			<?php $image = wp_get_attachment_image_src( $imageID, 'full' ); ?>
 			<?php $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true); ?>
-            <img src="<?php echo $image[0]; ?>" alt="<?php echo $alt_text; ?>" class="media-logos animated FadeInRight"/>
+
+			<?php if( $imageID ) : 
+				$image    = wp_get_attachment_image_src( $imageID, 'full' ); 
+				$alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
+			?>
+            	<img src="<?php echo $image[0]; ?>" alt="<?php echo $alt_text; ?>" class="media-logos animated FadeInRight"/>
+        	<?php endif; ?>
+
+        	<?php if( have_rows('images') ) : ?>
+				<div class="logos grid">	
+					<?php while( have_rows('images') ) : the_row(); ?>
+						<div class="item">
+						<?php 
+                             echo wp_get_attachment_image(get_sub_field('item'), 'full');
+						?>
+						</div>
+
+					<?php endwhile ?>
+				</div>		
+        	<?php endif; ?>	
    	    </div>
    </div>
 </section>
